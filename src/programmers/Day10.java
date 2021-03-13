170650e777da65b11497916dbbe391c990e720f5
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-13 17:21:42 +0900

    programmers day10

--- src/programmers/Day10.java
+++ src/programmers/Day10.java
@@ -1,2 +1,17 @@
-양기홍 바보
-
+boolean solution(String s) {
+        s=s.toLowerCase();
+        int pcnt=0;
+        int ycnt=0;
+        for(int i=0;i<s.length();i++){
+            if(s.substring(i,i+1).equals("p")){
+	            pcnt++;
+            }else if(s.substring(i,i+1).equals("y")){
+                ycnt++;
+            }
+         }
+        if(pcnt==ycnt){
+            return true;
+	}else{
+            return false;
+	}
+}
