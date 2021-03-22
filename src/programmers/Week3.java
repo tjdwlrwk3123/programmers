b2245b30586454466b63629c0e1ed721f653ca4d
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-22 16:14:54 +0900

    programmers day19-3

--- src/programmers/Week3.java
+++ src/programmers/Week3.java
@@ -251,4 +251,21 @@ public class Week3 {
         }
         return answer;
     }
+	/*
+	자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+	제한 조건
+	n은 10,000,000,000이하인 자연수입니다.
+	입출력 예
+	n		return
+	12345	[5,4,3,2,1]
+	 */
+	public int[] day19_3(long n) {
+        String numbers=n+"";
+        int[] answer = new int[numbers.length()];
+        for(int i=0;i<answer.length;i++){
+            answer[i]=Integer.parseInt(Character.toString(numbers.charAt(answer.length-1-i)));
+        }
+        return answer;
+    }
 }
