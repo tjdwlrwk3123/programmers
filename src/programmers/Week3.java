7f7763540f2bea21e4f2deb68e40959963232306
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-22 16:09:08 +0900

    programmers day19

--- src/programmers/Week3.java
+++ src/programmers/Week3.java
@@ -210,4 +210,45 @@ public class Week3 {
         answer=answer.substring(0,answer.length()-1);
         return answer;
     }
+	/*
+	함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+	다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+	제한 조건
+	x는 -10000000 이상, 10000000 이하인 정수입니다.
+	n은 1000 이하인 자연수입니다.
+	입출력 예
+	x		n		answer
+	2		5		[2,4,6,8,10]
+	4		3		[4,8,12]
+	-4		2		[-4, -8]
+	 */
+	public long[] day19_1(int x, int n) {
+        long k=(long)x; //x의 조건이 int의 범위를 벗어났기 때문에 long형으로 변환해야함(int와 int를 연산하면 결과도 int가 됨)
+        long[] answer = new long[n];
+        for(int i=0;i<n;i++){
+            answer[i]=k*(i+1);
+        }
+        return answer;
+    }
+	/*
+	행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 
+	2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+
+	제한 조건
+	행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+	입출력 예
+	arr1	arr2	return
+	[[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
+	[[1],[2]]	[[3],[4]]	[[4],[6]]
+	 */
+	public int[][] day19_2(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr1[i].length;j++){
+                answer[i][j]=arr1[i][j]+arr2[i][j];
+            }
+        }
+        return answer;
+    }
 }
