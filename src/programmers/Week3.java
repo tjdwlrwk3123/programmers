5074d2d1994667406f961cb2a0839d70461f1662
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-22 16:22:14 +0900

    programmers day19-4

--- src/programmers/Week3.java
+++ src/programmers/Week3.java
@@ -268,4 +268,25 @@ public class Week3 {
         }
         return answer;
     }
+	/*
+	임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+	n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+	
+	제한 사항
+	n은 1이상, 50000000000000 이하인 양의 정수입니다.
+	입출력 예
+	n		return
+	121		144
+	3		-1
+	 */
+	public long day19_4(long n) {
+        long answer = 0;
+        long root=(long)Math.sqrt(n); //sqrt : 제곱근을 구하는 함수
+        if(root*root==n){
+            answer=(root+1)*(root+1);
+        }else{
+            answer=-1;
+        }
+        return answer;
+    }
 }
