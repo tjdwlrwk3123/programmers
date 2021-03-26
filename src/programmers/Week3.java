f03292e9f44b31af2024bed75da90483b0756258
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-26 21:30:24 +0900

    programmers day22 카카오 코테 solved

--- src/programmers/Week3.java
+++ src/programmers/Week3.java
@@ -299,11 +299,37 @@ public class Week3 {
         }
         return answer;
     }
+	/*
+	슈퍼 게임 개발자 오렐리는 큰 고민에 빠졌다. 그녀가 만든 프랜즈 오천성이 대성공을 거뒀지만, 요즘 신규 사용자의 수가 급감한 것이다. 
+	원인은 신규 사용자와 기존 사용자 사이에 스테이지 차이가 너무 큰 것이 문제였다.
+
+	이 문제를 어떻게 할까 고민 한 그녀는 동적으로 게임 시간을 늘려서 난이도를 조절하기로 했다. 
+	역시 슈퍼 개발자라 대부분의 로직은 쉽게 구현했지만, 실패율을 구하는 부분에서 위기에 빠지고 말았다. 오렐리를 위해 실패율을 구하는 코드를 완성하라.
 	
-	public static int[] day21(int N, int[] stages) {
+	실패율은 다음과 같이 정의한다.
+	스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+	전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때, 
+	실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수를 완성하라.
+	
+	제한사항
+	스테이지의 개수 N은 1 이상 500 이하의 자연수이다.
+	stages의 길이는 1 이상 200,000 이하이다.
+	stages에는 1 이상 N + 1 이하의 자연수가 담겨있다.
+	각 자연수는 사용자가 현재 도전 중인 스테이지의 번호를 나타낸다.
+	단, N + 1 은 마지막 스테이지(N 번째 스테이지) 까지 클리어 한 사용자를 나타낸다.
+	만약 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저 오도록 하면 된다.
+	스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0 으로 정의한다.
+	
+	입출력 예
+	N	stages						result
+	5	[2, 1, 2, 6, 2, 4, 3, 3]	[3,4,2,1,5]
+	4	[4,4,4,4,4]					[4,1,2,3]
+	5	[2,1,2,4,2,4,3,3]			
+	 */
+	public int[] day21_day22(int N, int[] stages) {
         int[] answer = new int[N];
         
-        Arrays.sort(stages);
+        Arrays.sort(stages); //배열을 오름차순으로 정렬
         int tried=stages.length;
         int clearP=0;
         int stage=1;
@@ -314,43 +340,35 @@ public class Week3 {
             double failure=0.0;
             for(int i=clearP;i<stages.length;i++){
                 if(stage==stages[i]){
-                	System.out.println("같아");
                     failed++;
                     cnt++;
                 }else{
-                    failure=(double)failed/tried;
-                    System.out.println("실패율:"+failure);
-                    break;
+                    break; //오름차순으로 정렬했기 때문에 바로 나가도 됨
                 }
             }
-            System.out.println("////////////");
-            map.put(stage,failure);
-            tried=tried-failed;
-            clearP=cnt;
-            stage++;
+            
+            
+            //**************첫번째 시도때 틀렸던 이유************
+            if(tried==0) tried=1; //만약 5스테이지중 5스테이지에 도달한 사람이 없는경우 0을 0으로 나누게 되는 일을 방지
+            
+            
+            failure=(double)failed/tried; //실패율
+            map.put(stage,failure); //스테이지 별 실패율
+            tried=tried-failed; //도전자 수에서 실패한 사람 수를 빼기
+            clearP=cnt; //실패한 사람의 다음번째부터 for문을 돌리기 위한 변수 설정
+            stage++; //다음 스테이지로
         }
+        //해시맵을 value를 기준으로 오름차순 or 내림차순으로 정렬하는 방법
+        //특히 value가 int형이 아닌 double형이기 때문에  람다식을 이용해서 compare오버라이딩
+        List<Integer> keySetList = new ArrayList<>(map.keySet());
+        Collections.sort(keySetList, (o1, o2) -> (map.get(o2).compareTo(map.get(o1)))); //내림차순
         
-        Iterator<Integer> it= sortByValue(map).iterator();
         int i=0;
-        while(it.hasNext()) {
-        	answer[i]=it.next();
+        for(Integer key : keySetList) {
+            answer[i]=key;
+            i++;
         }
         
         return answer;
-    }
-	
-	public static List<Integer> sortByValue(final Map<Integer, Double> map){
-        List<Integer> list = new ArrayList<Integer>();
-        list.addAll(map.keySet());
-        Collections.sort(list,new Comparator<Object>(){
-			@SuppressWarnings("unchecked")
-			public int compare(Object o1,Object o2){
-                Object v1 = map.get(o1);
-                Object v2 = map.get(o2);                
-                return ((Comparable<Object>) v1).compareTo(v2);
-            }
-        });
-        Collections.reverse(list); // 주석시 오름차순
-        return list;
     }
 }
