90cdcc15edcc94ee58bf9cdaf6afb7cf0d59a1b2
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-28 17:40:13 +0900

    programmers day24

--- src/programmers/Week4.java
+++ src/programmers/Week4.java
@@ -1,5 +1,7 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class Week4 {
 	/*
 	정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
@@ -46,6 +48,64 @@ public class Week4 {
             }
         }
         answer[1]=n*m/answer[0]; //최소공배수 구하기(두 수의 곱을 최대공약수로 나눈수)
+        return answer;
+    }
+	/*
+	///////////////////////소수 찾기//////////////////
+	
+	1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+
+	소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+	(1은 소수가 아닙니다.)
+	
+	제한 조건
+	n은 2이상 1000000이하의 자연수입니다.
+	
+	입출력 예
+	n	result
+	10	4
+	5	3
+	 */
+	public int day24_1(int n) {
+        int answer = 0;
+        
+        for(int j=2;j<=n;j++){
+            boolean isPrime=true;
+            int num=j;
+            for(int i=2;i<=Math.sqrt(num);i++){ //num이 2거나 3인경우는 for문이 돌지 않고 나가지만 둘다 소수이기 때문에 괜찮다.
+                if(num%i==0){ //루트 num까지 가기 전에 나누어 떨어지는 수가 하나라도 나오면 소수가 아니다.
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                answer++;
+            }
+        }
+        return answer;
+    }
+	/*
+	 * 
+	문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+	s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+	
+	제한 사항
+	str은 길이 1 이상인 문자열입니다.
+	입출력 예
+	s			return
+	"Zbcdefg"	"gfedcbZ"
+	
+	 */
+	public String day24_2(String s) {
+        String answer ="";
+        
+        char[] letters=s.toCharArray();
+        Arrays.sort(letters); //char배열도 Arrays.sort를 쓸 수 있다.
+        
+        StringBuilder sb=new StringBuilder();
+        sb.append(letters); //어펜드로 char배열을 넣을 수 있다.
+        answer=sb.reverse().toString();
+        
         return answer;
     }
 }
