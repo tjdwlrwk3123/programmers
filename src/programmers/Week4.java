41d96b0eaf52f03c16196b2a7f4c89f5c60dcf06
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-29 17:01:25 +0900

    programmers day25

--- src/programmers/Week4.java
+++ src/programmers/Week4.java
@@ -1,5 +1,6 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Week4 {
@@ -106,6 +107,57 @@ public class Week4 {
         sb.append(letters); //어펜드로 char배열을 넣을 수 있다.
         answer=sb.reverse().toString();
         
+        return answer;
+    }
+	/*
+	당신은 폰켓몬을 잡기 위한 오랜 여행 끝에, 홍 박사님의 연구실에 도착했습니다. 
+	홍 박사님은 당신에게 자신의 연구실에 있는 총 N 마리의 폰켓몬 중에서 N/2마리를 가져가도 좋다고 했습니다.
+	홍 박사님 연구실의 폰켓몬은 종류에 따라 번호를 붙여 구분합니다. 따라서 같은 종류의 폰켓몬은 같은 번호를 가지고 있습니다. 
+	예를 들어 연구실에 총 4마리의 폰켓몬이 있고, 
+	각 폰켓몬의 종류 번호가 [3번, 1번, 2번, 3번]이라면 이는 3번 폰켓몬 두 마리, 1번 폰켓몬 한 마리, 2번 폰켓몬 한 마리가 있음을 나타냅니다. 
+	이때, 4마리의 폰켓몬 중 2마리를 고르는 방법은 다음과 같이 6가지가 있습니다.
+	
+	당신은 최대한 다양한 종류의 폰켓몬을 가지길 원하기 때문에, 최대한 많은 종류의 폰켓몬을 포함해서 N/2마리를 선택하려 합니다. 
+	N마리 폰켓몬의 종류 번호가 담긴 배열 nums가 매개변수로 주어질 때, 
+	N/2마리의 폰켓몬을 선택하는 방법 중, 가장 많은 종류의 폰켓몬을 선택하는 방법을 찾아, 
+	그때의 폰켓몬 종류 번호의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+	제한사항
+	nums는 폰켓몬의 종류 번호가 담긴 1차원 배열입니다.
+	nums의 길이(N)는 1 이상 10,000 이하의 자연수이며, 항상 짝수로 주어집니다.
+	폰켓몬의 종류 번호는 1 이상 200,000 이하의 자연수로 나타냅니다.
+	가장 많은 종류의 폰켓몬을 선택하는 방법이 여러 가지인 경우에도, 선택할 수 있는 폰켓몬 종류 개수의 최댓값 하나만 return 하면 됩니다.
+	입출력 예
+	nums			result
+	[3,1,2,3]		2
+	[3,3,3,2,2,4]	3
+	[3,3,3,2,2,2]	2
+	
+	
+	****다른사람의 풀이 : 해쉬셋에 담으면 중복이 제거되기 때문에 그 후 사이즈를 비교하여 리턴****
+	
+	 */
+	public int day25(int[] nums) {
+        int answer = 0;
+        if(nums.length==2){ //2마리일 경우는 무조건 1마리만 가져가게 됨.
+            return 1;
+        }
+        Arrays.sort(nums);
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        list.add(nums[0]);
+        int s=0;
+        for(int i=1;i<nums.length;i++){
+            if(list.get(s)==nums[i]){
+                continue;
+            }else{
+                list.add(nums[i]);
+                s++;
+            }
+            if(list.size()==nums.length/2){
+                break;
+            }
+        }
+        answer=list.size();
         return answer;
     }
 }
