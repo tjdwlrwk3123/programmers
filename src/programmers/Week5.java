ba51ea31cdf4588617db755ebcd755bfd872f213
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-04-02 19:12:13 +0900

    programmers day28

--- src/programmers/Week5.java
+++ src/programmers/Week5.java
@@ -1,5 +1,6 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -60,4 +61,71 @@ public class Week5 {
 	        this.prior=prior;
 	    }
 	}
+	/*
+	 * 
+	///////////Queue문제 -> 기능개발//////////////
+	프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+
+	또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 
+	이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+	
+	먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때 
+	각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+	
+	제한 사항
+	작업의 개수(progresses, speeds배열의 길이)는 100개 이하입니다.
+	작업 진도는 100 미만의 자연수입니다.
+	작업 속도는 100 이하의 자연수입니다.
+	배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
+	입출력 예
+	progresses					speeds				return
+	[93, 30, 55]				[1, 30, 5]			[2, 1]
+	[95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+	 */
+	public int[] day28(int[] progresses, int[] speeds) {
+        Queue<Progress> queue=new LinkedList<>();
+        ArrayList<Integer> finishList=new ArrayList<>();
+        for(int i=0;i<progresses.length;i++){
+            queue.add(new Progress(progresses[i],speeds[i])); //큐에 모든 작업들을 추가
+        }
+        int day=0;
+        while(!queue.isEmpty()){
+            int p= queue.peek().progress;
+            int s= queue.peek().speed;
+            int finished=0; //배열에 들어갈 완료 목록들
+            p+=s*day; //2번째루프 이후부터는 지나간 일수만큼 작업량을 더해줘야함
+            while(p<100){ //100보다 작을때까지 반복(100과 같아도 바로 폴)
+                p+=s;
+                day++;
+            }
+            queue.poll();
+            finished++;
+            while(!queue.isEmpty()){ //그 다음작업이 끝났는지 루프를 돌면서 확인(만약 큐가 끝나면 탈출)
+                int p2=queue.peek().progress;
+                int s2=queue.peek().speed;
+                p2+=s2*day;
+                if(p2>=100){
+                    queue.poll();
+                    finished++;
+                }else{
+                    break;
+                }
+            }
+            finishList.add(finished); //완료한 개수를 어레이리스트에 추가
+        }
+        int[] answer=new int[finishList.size()];
+        for(int i=0;i<finishList.size();i++){
+            answer[i]=finishList.get(i); //어레이리스트 배열을 다시 기본 배열에 옮기기
+        }
+        return answer;
+    }
+	class Progress{
+	    int progress;
+	    int speed;
+	    Progress(int progress,int speed){
+	        this.progress=progress;
+	        this.speed=speed;
+	    }
+	}
 }
+
