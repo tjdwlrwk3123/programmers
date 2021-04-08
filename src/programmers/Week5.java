cbf659fabfe89b16e9b63705f07820e81d54a5c8
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-04-08 21:18:14 +0900

    programmers day33

--- src/programmers/Week5.java
+++ src/programmers/Week5.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Week5 {
@@ -238,6 +239,45 @@ public class Week5 {
     		return "0";
     	}
     	return answer;
+    }
+    /*
+    ///////////////////더 맵게(priority queue문제)///////////////////
+	매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다. 
+	모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+
+	섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+	Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+	Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때,
+	모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+	
+	제한 사항
+	scoville의 길이는 2 이상 1,000,000 이하입니다.
+	K는 0 이상 1,000,000,000 이하입니다.
+	scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
+	모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+	
+	입출력 예
+	scoville				K		return
+	[1, 2, 3, 9, 10, 12]	7		2
+     */
+    public int day33(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> q=new PriorityQueue<Integer>(); //우선순위 큐 생성
+        for(int i=0;i<scoville.length;i++){
+            q.add(scoville[i]);
+        }
+        while(q.peek()<K){ //가장 작은 스코빌지수가 K보다 작은동안 계속
+            if(q.size()==1) return -1; //하지만 큐의 사이즈가 1일경우는 더이상 합칠수 없으므로 -1 리턴
+            int a=q.poll();
+            int b=q.poll();
+            int newScoville=a+b*2;
+            if(newScoville==0){ //두 수가 모두 0일때도 아무리 합쳐도 K이상 불가능
+                return -1;
+            }
+            q.add(newScoville); //큐에 새로 추가(우선순위로 인해 자리가 옮겨짐)
+            answer++;
+        }
+        return answer;
     }
 }
 
